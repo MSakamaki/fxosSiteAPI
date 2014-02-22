7e91930f7d15ec5b6009b2e90f4c3a877a6bfd77
MSakamaki <daigouzigai5@gmail.com>
2014-02-22 16:42:07 +0900

    event refresh

--- main.js
+++ main.js
@@ -19,19 +19,32 @@ restServer.use(
 
 
 /* rest function */
-var eventList = [
-{ 
-    title: "関東Firefox OS勉強会 3rd ごった煮",
-    dateFrom: "2014/02/01 19:00",
-    dateTo: "2014/02/01 20:00",
-    url: "http://www.zusaar.com/event/924003",
-    description: "関東FirefoxOS勉強会の3回目です。 まだまだ登壇者募集中です！ 会場 シナジーカフェ GMO Yours 東京都渋谷区桜丘町26番1号　セルリアンタワー 11階 参加費 無料 入場方法 セルリアンタワーのロビー（エスカレーターを上ったところ）で受付"
-},{ 
-    title: "関西Firefox OS勉強会 1st",
-    dateFrom: "2014/02/01 19:00",
-    dateTo: "2014/02/01 20:00",
-    url: "http://atnd.org/events/39457",
-    description: "遂に開発機が発売されたFirefox OSについての勉強会です。技術的な内容はもちろんのこと、Keon、PeakやFirefox OSをインストールしたNexus Sを実際に触ってみたり、そこから感じられるFirefox OS、スマートフォンの未来を話せたらと思います。"
+var eventList = [{
+    title: "FxOSコードリーディングミートアップ #5",
+    dateFrom: "2014/03/14 19:30",
+    dateTo: "2014/03/14 22:00",
+    url: "http://atnd.org/events/47848",
+    description: "2014年3月の「FxOSコードリーディングミートアップ #5」を開催します。"
+},{
+    title: "Firefox OS大喜利ハッカソン",
+    subtitle: "Twitter APIを使ってみよう",
+    dateFrom: "2014/03/15 09:30",
+    dateTo: "2014/03/15 19:00",
+    url: "http://atnd.org/events/47933",
+    description: "ちょっぴり新春 Firefox OS ハッカソンから2ヶ月、そろそろハッカソンがしたくなる季節ですね。\n\n今回のお題は「Twitter APIを使う」。先日のFirefox OS勉強会 6thで作成したTwitterクライアントを改良するもよし、新しくアプリを作るもよし。\n(もちろん、まったく関係ないものを書いても構いません。)"
+},{
+    title: "Gecko勉強会 その3",
+    subltitle: "ブラウザの仕組みを教わった上で、サクサク動くWebアプリの作り方を話し合う会",
+    dateFrom: "2014/03/05 19:30",
+    dateTo: "2014/03/05 21:30",
+    url: "http://atnd.org/events/47662",
+    description: "今回のテーマは「サクサク動くWebアプリの作り方」\n前半はMozilla Japanの @makoto_kato さんよりFirefoxの2D描画の仕組みをご紹介いただき、後半は「サクサク動くHTMLアプリの作り方」をテーマに参加している皆さんを交えてフリーディスカッションを行います。"
+},{
+    title: "FxOSコードリーディングミートアップ＃４",
+    dateFrom: "2014/02/22 13:00",
+    dateTo: "2014/02/22 18:00",
+    url: "http://atnd.org/event/fxoscodereading4",
+    description: "早くも四回目になりました\n\nFirefox OSはギークにリーチする端末にすると発表されて俄然ガジェオタ的に盛り上がってきましたよね！\n日本での発売は楽しみですがそれを待つまでもなくFxOSは触れます。今日もどこかでガジェット好きのみんながFxOSを触ってます。この機にちょっと中身も見てみませんか？"
 }];
 
 var menberList =[{
